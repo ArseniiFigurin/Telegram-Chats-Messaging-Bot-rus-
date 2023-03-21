34f633390c3555c00b470d27dfa6119a94ce64a0
Arsenii Figurin <newrecruitmentservice@gmail.com>
2023-03-21 14:22:12 -0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,16 +3,16 @@ import time
 
 class Config(object):
 
-    # Get from my.telegram.org (or @UseTGXBot)
+    # Get from my.telegram.org
     API_ID = int(os.environ.get("API_ID", "27401020")
 
 
-    # Get from my.telegram.org (or @UseTGXBot)
+    # Get from my.telegram.org
     API_HASH = os.environ.get("API_HASH", "3729fc432d01623f477178c4646df7c4")
     
     
     # Znachenie taimera
-    Znachenie taimera = os.environ.get("Znachenie taimera", "60")
+    Znachenie_taimera = os.environ.get("Znachenie_taimera", "60")
 
 
     # To record start time of bot
